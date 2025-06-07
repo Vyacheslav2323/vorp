c988bf20f547bbe2bfde4c0712c8e33901edd68a
Vyacheslav Park <vyacheslav.pak2323@gmail.com>
2025-06-07 17:42:13 +0900

    hosts for real

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tw6nwts6hnsfz#3f(e_sr17z*l*8kdt+2icwy^6j%*-^fv+opc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['vocabapp-1.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['vorp.onrender.com']
 
 
 # Application definition
